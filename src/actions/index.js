c05368704206af8f8654e80f83e3b611bcaa0d7a
gemaquejr <gemaquejr@hotmail.com>
2023-03-14 19:19:07 -0300

    Adiciona actions para o botao edit

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,8 @@ export const SAVE_EMAIL = 'SAVE_EMAIL';
 export const SAVE_WALLET = 'SAVE_WALLET';
 export const SAVE_CURRENT = 'SAVE_CURRENT';
 export const DELETE_WALLET = 'DELETE_WALLET';
+export const ENABLE_EXPENSE_EDITING = 'ENABLE_EXPENSE_EDITING';
+export const EDIT_EXPENSE = 'EDIT_EXPENSE';
 
 export const actionSaveEmail = (payload) => ({
   type: SAVE_EMAIL,
@@ -21,5 +23,15 @@ export const actionSaveCurrent = (payload) => ({
 
 export const actionDeleteWallet = (payload) => ({
   type: DELETE_WALLET,
+  payload,
+});
+
+export const enableExpenseEditing = (payload) => ({
+  type: ENABLE_EXPENSE_EDITING,
+  payload,
+});
+
+export const editExpense = (payload) => ({
+  type: EDIT_EXPENSE,
   payload,
 });
