0268157b65516a28a9822f833e773127384874ff
gemaquejr <gemaquejr@hotmail.com>
2023-03-14 21:03:24 -0300

    remove componente button de index.js em componentes

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
-import Button from './button';
 import Header from './header';
 import ExpenseForm from './expenseForm';
 import Table from './table';
 import DeleteButton from './deleteButton';
 
 export {
-  Button,
   Header,
   ExpenseForm,
   Table,
